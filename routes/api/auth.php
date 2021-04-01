c0cdf7f8e2a0a3ec55678297df3a90737a53c3af
karahman <jakarahman007@gmail.com>
2021-04-01 11:30:12 +0700

    move auth controller dir

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->name('auth.')->group(function () {

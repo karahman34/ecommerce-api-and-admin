22522bfb1901b96e83761a199b0aa7ccc006588e
karahman <jakarahman007@gmail.com>
2021-04-06 09:33:43 +0700

    add product details & related routes

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductsCollection;
 use App\Models\Product;
 use App\Utils\Transformer;
@@ -51,6 +52,50 @@ class ProductController extends Controller
         }
     }
 
+    /**
+     * Show Product details.
+     *
+     * @param   Product  $product
+     *
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function show(Product $product)
+    {
+        try {
+            $product->load(['thumbnail', 'images']);
+
+            return Transformer::success('Success to load product details data.', new ProductResource($product));
+        } catch (\Throwable $th) {
+            return Transformer::failed('Failed to load product details data.');
+        }
+    }
+
+    /**
+     * Show related Product.
+     *
+    *  @param   Request  $request
+     * @param   Product  $product
+     *
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function related(Request $request, Product $product)
+    {
+        try {
+            $limit = $request->input('limit', 8);
+
+            $relatedProducts = Product::with(['thumbnail', 'images'])
+                                        ->whereIn('category_id', $product->category_id)
+                                        ->paginate($limit);
+
+            return (new ProductsCollection($relatedProducts))
+                    ->additional(
+                        Transformer::skeleton(true, 'Success to load related product data.', null, true)
+                    );
+        } catch (\Throwable $th) {
+            return Transformer::failed('Failed to load related product data.');
+        }
+    }
+    
     /**
      * Get popular products list.
      *
--- routes/api/products.php
+++ routes/api/products.php
@@ -7,4 +7,6 @@ Route::prefix('products')->name('products.')->group(function () {
     Route::get('/', [ProductController::class, 'index'])->name('index');
     Route::get('/random', [ProductController::class, 'random'])->name('random');
     Route::get('/popular', [ProductController::class, 'popular'])->name('popular');
+    Route::get('/{product}', [ProductController::class, 'show'])->name('show');
+    Route::get('/{product}/related', [ProductController::class, 'related'])->name('related');
 });
